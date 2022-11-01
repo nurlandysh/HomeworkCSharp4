a54a31cf171ea5440221bbbf937a42c3dd6fd082
Шаймарданов Ильфат <ilfatsh@Sajmardanovs-MacBook-Pro.local>
2022-11-01 19:18:39 +0300

    Solved the task

---
+++ 27/Program.cs
@@ -0,0 +1,20 @@
+﻿// 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetSumDigits(int A)
+{
+    int result = 0;
+    while (A > 0)
+    {
+        result = result + (A % 10);
+        A = A / 10;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа {N} равна {GetSumDigits(N)}.");
